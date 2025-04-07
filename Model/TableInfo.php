5ae1b7c396ddb41daa3f9336f0ea8fa4af8ce7e9
J-Setzer <ticket@bizzworxx.de>
2025-04-07 07:52:40 +0200

    added imageSource and getTableImage()

--- Model/TableInfo.php
+++ Model/TableInfo.php
@@ -8,12 +8,14 @@ class TableInfo
     {
         $this->pkField = getPKFieldName($this->tn);
         $this->name = $tn;
-        
+
         $ti = getTableList(true, false)[$this->tn];
         $this->caption = $ti[0];
         $this->description = $ti[1];
         $this->icon = $ti[2];
         $this->group = $ti[3];
+        $this->imageSource = application_url($this->icon);
+
     }
 
     public readonly string $name;
@@ -22,4 +24,11 @@ class TableInfo
     public readonly string $description;
     public readonly string $icon;
     public readonly string $group;
+
+    public readonly string $imageSource;
+
+    public function getImage(): string
+    {
+        return "<img src=\"{$this->imageSource}\" style=\"height: 1.25em\" />";
+    }
 }
